cd1106efea6d0423cbc2be0fb7613b220e652b92
Salim <105727265+Salim-Flutter@users.noreply.github.com>
2023-11-25 21:26:17 +0530

    feat: continent filtering

--- lib/country_phone_field.dart
+++ lib/country_phone_field.dart
@@ -43,6 +43,9 @@ class CountryPhoneField extends StatefulWidget {
     this.errorColor,
     this.borderWidth,
     this.focusedBorderWidth,
+    this.continents,
+    this.searchText,
+    this.countrySearchController,
   });
 
   final FocusNode? focusNode;
@@ -76,6 +79,9 @@ class CountryPhoneField extends StatefulWidget {
   final Color? errorColor;
   final double? borderWidth;
   final double? focusedBorderWidth;
+  final List<Continent>? continents;
+  final String? searchText;
+  final TextEditingController? countrySearchController;
 
   @override
   State<CountryPhoneField> createState() => _CountryPhoneFieldState();
@@ -83,17 +89,25 @@ class CountryPhoneField extends StatefulWidget {
 
 class _CountryPhoneFieldState extends State<CountryPhoneField> {
   late CountryModel selectedCountry;
+  late TextEditingController _countrySearchController;
+  late TextEditingController _phoneController;
 
   void _getCountry() async {
+    _countrySearchController =
+        widget.countrySearchController ?? TextEditingController();
+    _phoneController = widget.controller ?? TextEditingController();
     if (widget.initialCountryCode != null) {
-      selectedCountry =
-          kAllCountries.findByCountryCode(widget.initialCountryCode!) ??
-              await Utility.getInitialCountry;
+      selectedCountry = kAllCountries
+              .allowedCountries(widget.continents)
+              .findByCountryCode(widget.initialCountryCode!) ??
+          await Utility.getInitialCountry(widget.continents);
     } else if (widget.initialISOCode != null) {
-      selectedCountry = kAllCountries.findByISOCode(widget.initialISOCode!) ??
-          await Utility.getInitialCountry;
+      selectedCountry = kAllCountries
+              .allowedCountries(widget.continents)
+              .findByISOCode(widget.initialISOCode!) ??
+          await Utility.getInitialCountry(widget.continents);
     } else {
-      selectedCountry = await Utility.getInitialCountry;
+      selectedCountry = await Utility.getInitialCountry(widget.continents);
     }
     AppLog(selectedCountry);
   }
@@ -104,22 +118,126 @@ class _CountryPhoneFieldState extends State<CountryPhoneField> {
     _getCountry();
   }
 
-  InputBorder _getBorder(
-    Color color,
-    double width,
-  ) =>
-      OutlineInputBorder(
-        borderRadius: widget.borderRadius ?? BorderRadius.circular(12),
-        borderSide: BorderSide(
-          color: color,
-          width: width,
+  List<CountryModel> get filteredCountries {
+    final countryData = kAllCountries.allowedCountries(widget.continents);
+    final query = _countrySearchController.text.trim().replaceAll('+', '');
+    if (query.isEmpty) return countryData;
+
+    /// TODO: Need to do Translations for Key
+    return countryData
+        .where((e) =>
+            [e.name, e.countryCode.toString(), e.isoCode].contains(query))
+        .toList();
+  }
+
+  Future<void> _changeCountry() async {
+    await showDialog<void>(
+      context: context,
+      useRootNavigator: false,
+      builder: (context) => Dialog(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  suffixIcon: const Icon(Icons.search),
+                  labelText: widget.searchText ?? 'Search',
+                ),
+                controller: _countrySearchController,
+                onChanged: (value) {
+                  setState(() {});
+                },
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: filteredCountries.length,
+                  itemBuilder: (ctx, index) => Column(
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(
+                          filteredCountries[index].name,
+                          style: const TextStyle(fontWeight: FontWeight.w700),
+                        ),
+                        trailing: Text(
+                          '+${filteredCountries[index].countryCode}',
+                          style: const TextStyle(fontWeight: FontWeight.w700),
+                        ),
+                        onTap: () {
+                          selectedCountry = filteredCountries[index];
+
+                          if (widget.onCountryChanged != null) {
+                            widget.onCountryChanged!(
+                              '',
+                              PhoneNumber(
+                                countryCode: selectedCountry.countryCode,
+                                isoCode: selectedCountry.isoCode,
+                                number: _phoneController.text,
+                              ),
+                            );
+                          }
+
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      const Divider(thickness: 1),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+    setState(() {});
+  }
+
+  InputBorder _getBorder(Color color, double width) => OutlineInputBorder(
+        borderRadius: widget.borderRadius ?? BorderRadius.circular(12),
+        borderSide: BorderSide(color: color, width: width),
       );
+
+  Widget _buildFlagsButton() => InkWell(
+    // borderRadius: widget.dropdownDecoration.borderRadius as BorderRadius?,
+    onTap: _changeCountry,
+    child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          // Image.asset(
+          //   'assets/flags/${selectedCountry['code']!.toLowerCase()}.png',
+          //   package: 'intl_phone_field',
+          //   width: 32,
+          // ),
+          const SizedBox(width: 8),
+          FittedBox(
+            child: Text(
+              '+${selectedCountry.countryCode}',
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+                // color: widget.countryCodeTextColor,
+              ),
+            ),
+          ),
+          // if (widget.showDropdownIcon) ...[
+          const Icon(Icons.arrow_drop_down, color: Colors.black),
+          const SizedBox(width: 8),
+          // ],
+          // SizedBox(width: 8),
+        ],
+      ),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) => TextFormField(
         key: widget.key,
-        controller: widget.controller,
+        controller: _phoneController,
         focusNode: widget.focusNode,
         autofocus: widget.autoFocus,
         enabled: widget.enabled,
@@ -139,6 +257,7 @@ class _CountryPhoneFieldState extends State<CountryPhoneField> {
         },
         onTap: widget.onTap,
         decoration: InputDecoration(
+          prefixIcon: _buildFlagsButton(),
           enabled: widget.enabled,
           border: _getBorder(Colors.grey, 1),
           enabledBorder: _getBorder(
--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -1,4 +1,5 @@
 import 'package:country_phone_field/country_phone_field.dart';
+import 'package:flutter/foundation.dart';
 
 extension FindPhone on List<CountryModel> {
   CountryModel? findByCountryCode(int countryCode) => cast<CountryModel?>()
@@ -6,4 +7,14 @@ extension FindPhone on List<CountryModel> {
 
   CountryModel? findByISOCode(String isoCode) => cast<CountryModel?>()
       .firstWhere((e) => e!.isoCode == isoCode, orElse: () => null);
+
+  List<CountryModel> allowedCountries(List<Continent>? continents) {
+    if (continents == null || continents.isEmpty) {
+      return this;
+    }
+    if (listEquals(continents, Continent.values)) {
+      return this;
+    }
+    return where((e) => continents.contains(e.continent)).toList();
+  }
 }
